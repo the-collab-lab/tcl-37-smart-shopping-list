d47868a77e6ab3d5b42db0318e06ee3319ec01eb
Rafa Brown <shane.rafael@zohomail.com>
2022-01-11 15:16:40 -0700

    Create custom link component that displays bold when active

--- src/components/AddItemView.jsx
+++ src/components/AddItemView.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export const AddItemView = () => {
   return (
     <div>
       <h1>Add Item View</h1>
-      <div className="footer">
-        <Link to="/" className="bold">
-          List
-        </Link>
-        <Link to="/add" className="bold">
-          Add New Item
-        </Link>
-      </div>
     </div>
   );
 };
