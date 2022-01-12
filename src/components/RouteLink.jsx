09d7e1bd7acde0668f447021804ebd44aed40544
Devarat Patel <devaratpatel1@gmail.com>
2022-01-11 20:50:32 -0500

    feat: ready for team review

--- src/components/RouteLink.jsx
+++ src/components/RouteLink.jsx
@@ -1,17 +1,14 @@
 import React, { Fragment } from 'react';
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
-export const RouteLink = ({ children, to, ...props }) => {
+export const RouteLink = ({ children, to }) => {
   let resolved = useResolvedPath(to);
+  // console.log('props---->', to);
   let match = useMatch({ path: resolved.pathname, end: true });
-
+  // console.log('children-->', children);
   return (
     <Fragment>
-      <Link
-        style={{ fontWeight: match ? 'bold' : 'normal' }}
-        to={to}
-        {...props}
-      >
+      <Link style={{ fontWeight: match ? 'bold' : 'normal' }} to={to}>
         {children}
       </Link>
     </Fragment>
