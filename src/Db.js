236f0cfb97d56e7591f5d9a509b353a9ec05e2a7
Viviana Davila <vdavila@ucsd.edu>
2022-01-12 12:17:03 -0800

    pushed document data to state

--- src/Db.js
+++ src/Db.js
@@ -4,6 +4,27 @@ import db from './firebase';
 import { collection, onSnapshot } from 'firebase/firestore';
 
 export default function Db() {
+  const [docs, setDocs] = useState([]);
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, 'users'), (snapshot) => {
+      const snapshotDocs = [];
+      snapshot.forEach((doc) => {
+        snapshotDocs.push({ ...doc.data(), id: doc.id });
+        console.log(doc.id);
+      });
+      setDocs(snapshotDocs);
+    });
+  }, []);
+  // console.log(docs.forEach((doc) => console.log(doc.data())));
+  console.log(docs);
+
+  //doc.data() returns a data object from the db.
+  //   {
+  //     "first": "Ada",
+  //     "last": "Lovelace",
+  //     "born": 1815
+  // }
+
   const sendData = async () => {
     try {
       const docRef = await addDoc(collection(db, 'users'), {
@@ -17,27 +38,19 @@ export default function Db() {
     }
   };
 
-  const [docs, setDocs] = useState([]);
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, 'users'), (snapshot) => {
-      const snapshotDocs = [];
-      snapshot.forEach((doc) => snapshotDocs.push(doc));
-      setDocs(snapshotDocs);
-    });
-  }, []);
-  console.log(docs.forEach((doc) => console.log(doc.data())));
-
-  //doc.data() returns a data object from the db.
-  //   {
-  //     "first": "Ada",
-  //     "last": "Lovelace",
-  //     "born": 1815
-  // }
-
   return (
     <div>
       Send data:
       <button onClick={sendData}>Send data to database</button>
+      <ul>
+        {/* {
+          docs.map(doc => {
+            return (
+              <li>{doc.last}</li>
+            )
+          })
+        } */}
+      </ul>
     </div>
   );
 }
