3b7cf1930b2dd74d697781567e465b0161d25b39
danainjax <danainjax@gmail.com>
2022-01-12 15:38:58 -0500

    updated collection and returning data from firestore

--- src/Db.js
+++ src/Db.js
@@ -6,31 +6,23 @@ import { collection, onSnapshot } from 'firebase/firestore';
 export default function Db() {
   const [docs, setDocs] = useState([]);
   useEffect(() => {
-    const unsub = onSnapshot(collection(db, 'users'), (snapshot) => {
+    const unsub = onSnapshot(collection(db, 'list'), (snapshot) => {
       const snapshotDocs = [];
       snapshot.forEach((doc) => {
         snapshotDocs.push({ ...doc.data(), id: doc.id });
-        console.log(doc.id);
       });
       setDocs(snapshotDocs);
     });
+    return () => {
+      unsub();
+    };
   }, []);
-  // console.log(docs.forEach((doc) => console.log(doc.data())));
-  console.log(docs);
-
-  //doc.data() returns a data object from the db.
-  //   {
-  //     "first": "Ada",
-  //     "last": "Lovelace",
-  //     "born": 1815
-  // }
 
   const sendData = async () => {
     try {
-      const docRef = await addDoc(collection(db, 'users'), {
-        first: 'Ada',
-        last: 'Lovelace',
-        born: 1815,
+      const docRef = await addDoc(collection(db, 'list'), {
+        item: 'green beans',
+        quantity: '1 pound',
       });
       console.log('Document written with ID: ', docRef.id);
     } catch (e) {
@@ -43,13 +35,13 @@ export default function Db() {
       Send data:
       <button onClick={sendData}>Send data to database</button>
       <ul>
-        {/* {
-          docs.map(doc => {
-            return (
-              <li>{doc.last}</li>
-            )
-          })
-        } */}
+        {docs.map((doc) => {
+          return (
+            <li key={doc.id}>
+              {doc.item} : {doc.quantity}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
