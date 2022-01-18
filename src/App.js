7ae9ed410c741a305df0146b1dc7125a53f8f4b3
danainjax <danainjax@gmail.com>
2022-01-17 20:23:04 -0500

    added shopping list utils

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AddItemView } from './components/AddItemView';
 import { ListView } from './components/ListView';
+import Home from './components/Home';
 import { RouteLink } from './components/RouteLink';
 import './App.css';
 
@@ -10,7 +11,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<ListView />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/list" element={<ListView />} />
           <Route path="/add" element={<AddItemView />} />
         </Routes>
         <nav className="footer">
