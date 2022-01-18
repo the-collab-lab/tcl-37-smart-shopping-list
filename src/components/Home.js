8e3a9e26745ca5cb297bf5acfd69877ad8c2bde6
Rafa Brown <shane.rafael@zohomail.com>
2022-01-17 18:29:44 -0700

    Navigate user to list page once token generated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,28 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import {
   getToken,
   words,
   calculateEstimate,
 } from '@the-collab-lab/shopping-list-utils';
 
-const generateToken = () => {
-  const token = getToken();
-  console.log(token);
-  localStorage.token = token;
-};
-
 const Home = () => {
+  let navigate = useNavigate();
+
+  const [token, setToken] = useState('');
+
+  const generateToken = () => {
+    const token = getToken();
+    console.log(token);
+    localStorage.token = token;
+    setToken(token);
+  };
+
+  useEffect(() => {
+    if (token) navigate('/list');
+  }, [token, navigate]);
+
   return <button onClick={() => generateToken()}>Generate Token</button>;
 };
 
