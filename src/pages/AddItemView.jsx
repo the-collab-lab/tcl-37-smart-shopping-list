7fd7d9b7262514ef19344a4ef6b98d15612a0818
Devarat Patel <devaratpatel1@gmail.com>
2022-01-19 18:54:35 -0500

    Revert "feat: made new component for addItemForm"
    This reverts commit dcba74e0778d8fe3b974618c4f7742af1af78253.

--- src/pages/AddItemView.jsx
+++ src/pages/AddItemView.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import db from '../lib/firebase';
 import { collection, addDoc } from 'firebase/firestore';
-import AddItemForm from '../components/AddItemForm';
+
 export const AddItemView = () => {
   const [inputs, setInputs] = useState({ 'last purchase date': null });
 
@@ -31,11 +31,50 @@ export const AddItemView = () => {
   return (
     <div>
       <h1>Smart Shopping List</h1>
-      <AddItemForm
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        inputs={inputs}
-      />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Item name</label>
+          <input
+            type="text"
+            required
+            id="name"
+            name="item"
+            value={inputs.item || ''}
+            onChange={handleChange}
+          />
+        </div>
+        <h3 htmlFor="options">How soon will you buy this again?</h3>
+        <div>
+          <input
+            type="radio"
+            value="7"
+            id="soon"
+            name="days"
+            onChange={handleChange}
+          />
+          <label htmlFor="soon">Soon</label>
+
+          <input
+            type="radio"
+            value="14"
+            id="kind-soon"
+            name="days"
+            onChange={handleChange}
+          />
+          <label htmlFor="kind-soon">Kind of Soon</label>
+
+          <input
+            type="radio"
+            value="30"
+            id="not-soon"
+            name="days"
+            onChange={handleChange}
+          />
+          <label htmlFor="not-soon">Not Soon</label>
+
+          <button type="submit">Add Item</button>
+        </div>
+      </form>
     </div>
   );
 };
