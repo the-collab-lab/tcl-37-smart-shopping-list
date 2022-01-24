cad2dc6d8031aa84de7c0f13491c39ee6e1735c8
Devarat Patel <devaratpatel1@gmail.com>
2022-01-24 15:06:00 -0500

    feat: removed console.logs

--- src/pages/ListView.jsx
+++ src/pages/ListView.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { List } from '../components/List';
-import { Home } from '../components/Home';
 import { useNavigate } from 'react-router-dom';
 
 export const ListView = ({ token }) => {
-  console.log('token: ', token.token);
   let navigate = useNavigate();
 
   function handleClick() {
     navigate('/');
-    console.log('---->');
   }
 
   return (
