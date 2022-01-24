1fc903fc10bef60f6e8297d7b48b181e0eeac1f7
Devarat Patel <devaratpatel1@gmail.com>
2022-01-24 15:04:56 -0500

    feat: added redirect button incase token not generated

--- src/pages/ListView.jsx
+++ src/pages/ListView.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { List } from '../components/List';
 import { Home } from '../components/Home';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const ListView = ({ token }) => {
   console.log('token: ', token.token);
-  // let navigate = useNavigate();
+  let navigate = useNavigate();
 
-  return <div>{token ? <List token={token} /> : <Home />}</div>;
+  function handleClick() {
+    navigate('/');
+    console.log('---->');
+  }
+
+  return (
+    <div>
+      {token ? (
+        <List token={token} />
+      ) : (
+        <button onClick={handleClick}>Go back to Home</button>
+      )}
+    </div>
+  );
 };
