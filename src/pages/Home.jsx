712ff1954a8c6e568ef3a89a03a0a253257201cf
danainjax <danainjax@gmail.com>
2022-01-25 16:05:57 -0500

    passed token as a prop to JoinList component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export const Home = (props) => {
       ) : (
         <button onClick={() => generateToken()}>Create a new list</button>
       )}
-      <JoinList />
+      <JoinList token={token} />
     </div>
   );
 };
