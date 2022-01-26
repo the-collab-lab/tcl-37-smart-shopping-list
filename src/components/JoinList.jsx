32f27b951f60a4b58e6620685b61f5d42c05474d
danainjax <danainjax@gmail.com>
2022-01-26 14:31:05 -0500

    captured token input value

--- src/components/JoinList.jsx
+++ src/components/JoinList.jsx
@@ -1,14 +1,22 @@
-export const JoinList = ({ token }) => {
-  const joinList = (token) => {
-    console.log(token);
-    //grab the token and then use it to view the appropriate list
+import { useState } from 'react';
+
+export const JoinList = () => {
+  const [token, setToken] = useState('');
+  const joinList = (e) => {
+    localStorage.setItem('token', token);
+    //  use it to view the appropriate list
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setToken(value);
   };
   return (
     <>
       <p> -or- </p>
       <p> Join an existing shopping list by entering a three word token</p>
       <p> Share token </p>
-      <input type="text" value={token} />
+      <input type="text" value={token} onChange={handleChange} />
       <br />
       <br />
       <button type="submit" onClick={joinList} token={token}>
