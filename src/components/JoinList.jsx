38f6b92ec4bea4074cc0f15314c224b69213e421
danainjax <danainjax@gmail.com>
2022-01-26 19:03:09 -0500

    updated token passing to JoinList

--- src/components/JoinList.jsx
+++ src/components/JoinList.jsx
@@ -1,31 +1,33 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export const JoinList = () => {
-  const [token, setToken] = useState('');
+export const JoinList = (props) => {
+  console.log(props);
+  const [tokenInput, setTokenInput] = useState('');
 
   let navigate = useNavigate();
 
   const onClick = (e) => {
-    localStorage.setItem('token', token);
-    navigate('/list');
+    localStorage.setItem('token', tokenInput);
+    props.setToken(tokenInput);
+    if (tokenInput) navigate('/list');
 
     //  use it to view the appropriate list
   };
 
   const handleChange = (e) => {
     const value = e.target.value;
-    setToken(value);
+    setTokenInput(value);
   };
   return (
     <>
       <p> -or- </p>
       <p> Join an existing shopping list by entering a three word token</p>
       <p> Share token </p>
-      <input type="text" value={token} onChange={handleChange} />
+      <input type="text" value={tokenInput} onChange={handleChange} />
       <br />
       <br />
-      <button type="submit" onClick={onClick} token={token}>
+      <button type="submit" onClick={onClick}>
         Join an existing list
       </button>
     </>
