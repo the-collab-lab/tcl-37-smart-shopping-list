e17da1b2c0eee2bbc8685b5243693a0d92df87c8
Rafa Brown <shane.rafael@zohomail.com>
2022-01-31 10:44:20 -0700

    Add text & nav-button to List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
 import db from '../lib/firebase';
 import { collection } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 
 export const List = ({ token }) => {
+  let navigate = useNavigate();
   const [value, loading, error] = useCollection(collection(db, token), {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
@@ -13,12 +15,19 @@ export const List = ({ token }) => {
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
       Collection
-      {value && (
+      {value && value.docs.length > 0 ? (
         <ul>
           {value.docs.map((doc) => (
             <li key={doc.id}>{doc.data().item}</li>
           ))}
         </ul>
+      ) : (
+        <Fragment>
+          <p>Your shopping list is currently empty.</p>
+          <button type="button" onClick={() => navigate('/add')}>
+            ADD YOUR FIRST ITEM
+          </button>
+        </Fragment>
       )}
     </div>
   );
