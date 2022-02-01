af8acd1596242ec63a0c8516ccaa13e21ca1dd0a
Viviana Davila <vdavila@ucsd.edu>
2022-02-01 12:16:09 -0800

    captured document data in handlechange

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import db from '../lib/firebase';
-import { collection } from 'firebase/firestore';
-import { useCollection } from 'react-firebase-hooks/firestore';
+import { collection, doc } from 'firebase/firestore';
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
 import moment from 'moment';
 
 export const List = ({ token }) => {
@@ -9,14 +9,36 @@ export const List = ({ token }) => {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
 
+  const [itemId, setItemId] = useState(' ');
+
+  const [docValue] = useDocument(doc(db, token, itemId), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
+
+  console.log(docValue);
+
   const [checkedItems, setCheckedItems] = useState({});
 
-  const handleChange = (e) => {
+  const handleChange = (data, e) => {
     setCheckedItems({ ...checkedItems, [e.target.name]: e.target.value });
-    console.log(moment().format());
+    // console.log(moment().format());
+    console.log(data.id);
+    console.log(data.data());
+
+    setItemId(data.id);
+
+    // const itemRef = db.collection(token).doc(data.id);
+
+    console.log(itemId);
   };
 
-  console.log(checkedItems);
+  // console.log(checkedItems);
+
+  // value && (console.log(value.docs.map((doc) => {
+  //   return doc.data();
+  // })));
+
+  // console.log(new Date);
 
   return (
     <div>
@@ -33,7 +55,7 @@ export const List = ({ token }) => {
                 name={doc.id}
                 id={doc.id}
                 value={doc.data().item}
-                onChange={handleChange}
+                onChange={(e) => handleChange(doc, e)}
               />
               <label>{doc.data().item}</label>
             </li>
