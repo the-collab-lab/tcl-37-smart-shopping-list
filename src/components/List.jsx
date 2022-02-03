b5de4b0e09be176215e9d5a4781319347b89d0d0
Viviana Davila <vdavila@ucsd.edu>
2022-02-03 15:43:06 -0800

    calculate time difference and checked conditional

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import db from '../lib/firebase';
 import { collection, doc, updateDoc } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
@@ -10,6 +10,31 @@ export const List = ({ token }) => {
   });
 
   const [checkedItems, setCheckedItems] = useState({});
+  // const [timeNow, setTimeNow] = useState("");
+
+  // useEffect(() =>{
+  //   setTimeNow(moment().format());
+  //   console.log(timeNow);
+  // }, []);
+
+  // const timeNow = moment();
+  // console.log(timeNow);
+
+  const calcTimeDiff = (purchasedTime) => {
+    let timeNow = moment().format();
+    // let purchaseTime = "2022-02-01T12:50:22-09:00"
+    // const duration = moment.duration(x.diff(y));
+    var date1 = moment(timeNow, 'YYYYMMDD HH:mm:ss');
+    var date2 = moment(purchasedTime, 'YYYYMMDD HH:mm:ss');
+
+    console.log(date1.diff(date2, 'hours') + 'hrs'); // "7d"
+    let timeDiff = date1.diff(date2, 'hours');
+    console.log(timeDiff);
+
+    return timeDiff < 24;
+  };
+
+  // calcTimeDiff();
 
   const updateDocument = async (id) => {
     // debugger;
@@ -35,11 +60,13 @@ export const List = ({ token }) => {
             <li key={doc.id}>
               <input
                 type="checkbox"
-                // checked={doc.data().purchased_date ? false : true}
+                checked={calcTimeDiff(doc.data().purchased_date)}
+                disabled={calcTimeDiff(doc.data().purchased_date)}
                 name={doc.id}
                 id={doc.id}
-                value={doc.data().item}
+                // value={doc.data().item}
                 onClick={(e) => handleClick(doc, e)}
+                onChange={(e) => handleClick(doc, e)}
               />
               <label>{doc.data().item}</label>
             </li>
