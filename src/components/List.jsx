d9cf005a5996e5e48d6863b064b9022be907dbe3
Viviana Davila <vdavila@ucsd.edu>
2022-02-07 13:07:18 -0800

    filtered array then mapped results

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import db from '../lib/firebase';
 import { collection, doc, updateDoc } from 'firebase/firestore';
@@ -10,6 +10,7 @@ export const List = ({ token }) => {
   const [value, loading, error] = useCollection(collection(db, token), {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
+  let [filterText, setFilterText] = useState('');
 
   const calcTimeDiff = (purchasedTime) => {
     const timeNow = moment().format();
@@ -31,28 +32,43 @@ export const List = ({ token }) => {
     updateDocument(doc.id);
   };
 
+  const handleFilterChange = (e) => {
+    let text = e.target.value;
+
+    setFilterText(e.target.value);
+  };
+
   return (
     <div className="welcoming">
       <h1>Smart Shopping List</h1>
       <strong> Shareable List Token : {token} </strong>
+      <input
+        placeholder="Start typing here..."
+        value={filterText}
+        onChange={handleFilterChange}
+      />
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
       {value && value.docs.length > 0 ? (
         <ul className="collection-list">
-          {value.docs.map((doc) => (
-            <li key={doc.id}>
-              <input
-                type="checkbox"
-                checked={calcTimeDiff(doc.data().purchased_date)}
-                disabled={calcTimeDiff(doc.data().purchased_date)}
-                name={doc.id}
-                id={doc.id}
-                onClick={() => handleClick(doc)}
-                onChange={() => handleClick(doc)}
-              />
-              <label htmlFor={doc.id}>{doc.data().item}</label>
-            </li>
-          ))}
+          {value.docs
+            .filter((doc) =>
+              doc.data().item.toLowerCase().includes(filterText.toLowerCase()),
+            )
+            .map((doc) => (
+              <li key={doc.id}>
+                <input
+                  type="checkbox"
+                  checked={calcTimeDiff(doc.data().purchased_date)}
+                  disabled={calcTimeDiff(doc.data().purchased_date)}
+                  name={doc.id}
+                  id={doc.id}
+                  onClick={() => handleClick(doc)}
+                  onChange={() => handleClick(doc)}
+                />
+                <label htmlFor={doc.id}>{doc.data().item}</label>
+              </li>
+            ))}
         </ul>
       ) : (
         <div>
