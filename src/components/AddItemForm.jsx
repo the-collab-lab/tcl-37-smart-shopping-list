cea12ce013db1391070e77fc321e6936f62150fa
danainjax <danainjax@gmail.com>
2022-02-27 08:31:33 -0500

    centered soon buttons

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddItemForm = ({ handleChange, handleSubmit, inputs }) => {
   return (
-    <div>
+    <div className="input-item">
       <form onSubmit={handleSubmit}>
         <div>
           <h3>Item Name:</h3>
@@ -15,43 +15,45 @@ const AddItemForm = ({ handleChange, handleSubmit, inputs }) => {
             onChange={handleChange}
           />
         </div>
-        <h3>How soon will you buy this again?</h3>
         <div className="how-soon-div">
-          <label className="soon-box" htmlFor="soon">
-            <input
-              className="soon"
-              defaultChecked
-              type="radio"
-              value={7}
-              id="soon"
-              name="days"
-              onChange={handleChange}
-            />
-            Soon
-          </label>
-          <label className="kind-of-soon-box" htmlFor="kind-soon">
-            <input
-              className="kind-soon"
-              type="radio"
-              value={14}
-              id="kind-soon"
-              name="days"
-              onChange={handleChange}
-            />
-            Kind of Soon
-          </label>
+          <h3>How soon will you buy this again?</h3>
+          <div className="soon-buttons">
+            <label className="soon-box" htmlFor="soon">
+              <input
+                className="soon"
+                defaultChecked
+                type="radio"
+                value={7}
+                id="soon"
+                name="days"
+                onChange={handleChange}
+              />
+              Soon
+            </label>
+            <label className="kind-of-soon-box" htmlFor="kind-soon">
+              <input
+                className="kind-soon"
+                type="radio"
+                value={14}
+                id="kind-soon"
+                name="days"
+                onChange={handleChange}
+              />
+              Kind of Soon
+            </label>
 
-          <label className="not-soon-box" htmlFor="not-soon">
-            <input
-              className="not-soon"
-              type="radio"
-              value={30}
-              id="not-soon"
-              name="days"
-              onChange={handleChange}
-            />
-            Not Soon
-          </label>
+            <label className="not-soon-box" htmlFor="not-soon">
+              <input
+                className="not-soon"
+                type="radio"
+                value={30}
+                id="not-soon"
+                name="days"
+                onChange={handleChange}
+              />
+              Not Soon
+            </label>
+          </div>
         </div>
         <button type="submit">Add Item</button>
       </form>
