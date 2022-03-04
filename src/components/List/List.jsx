b149a235ffb6c5dfbfec26267460400f8933464f
danainjax <danainjax@gmail.com>
2022-03-04 07:22:03 -0500

    centered loader spinner with mat ui Stack

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Stack } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import db from '../../lib/firebase';
 import {
@@ -137,7 +137,9 @@ const List = ({ token }) => {
           </ul>
         </section>
       ) : loading ? (
-        <CircularProgress />
+        <Stack alignItems="center">
+          <CircularProgress />
+        </Stack>
       ) : (
         <section className="welcome">
           <p>
